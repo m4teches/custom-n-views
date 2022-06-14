80f0c3d833faf07e07a784209194f4781cfcfaee
m4teches <52996882+m4teches@users.noreply.github.com>
2022-06-14 10:12:03 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <NInput />
+</template>
+
+<script>
+import NInput from "./components/NInput.vue";
+export default {
+  name: "App",
+  components: {
+    NInput: NInput,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input
+      @keyup="textSearch($event)"
+      v-bind="$attrs"
+      type="text"
+      placeholder="Write your data..."
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NInput",
+  data: function () {
+    return {
+      timer: undefined,
+    };
+  },
+  methods: {
+    textSearch(e) {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        console.log("searching...");
+      }, 1500);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  border: none;
+  border-bottom: 1px solid grey;
+  outline: none;
+  width: 100%;
+}
+input::plcaholder {
+  color: green;
+}
+</style>
